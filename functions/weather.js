8c4b265d39032f22421b47f40109fdd4f20c8bac
Simon Larsson <Simon.Larsson@akqa.com>
2020-08-25 14:49:14 +0200

    Add weather function.

---
+++ functions/weather.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+const endpoint = 'https://api.met.no/weatherapi/locationforecast/2.0/compact.json';
+const lat = '56.264373779296875';
+const lon = '10.310287475585938';
+
+exports.handler = async () => {
+    const res = await axios.get(`${endpoint}?lat=${lat}&lon=${lon}`);
+    const weather = res.data.properties.timeseries[0].data.instant;
+    return {
+        statusCode: 200,
+        body: JSON.stringify(weather)
+    };
+}
